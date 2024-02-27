b3d6f4d007234e2c255190209a200a350d9fb01f
NukLaochaem <nuklaochaem@gmail.com>
2024-02-27 07:08:51 +0100

    deploy

--- save_json.js
+++ save_json.js
@@ -1,17 +1,17 @@
 const fs = require("fs");
 const path = require("path");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
 
-const save = (favNumber) => {
+const save = async (favNumber) => {
   console.log("saving");
-  fs.writeFile(
-    path.join(__dirname, ".", "number.json"),
-    JSON.stringify(favNumber, null, 2),
-    (error) => {
-      if (error) {
-        throw error;
-      }
-    }
-  );
+  await s3
+    .putObject({
+      Body: JSON.stringify(favNumber, null, 2),
+      Bucket: "cyclic-extinct-poncho-eel-ap-northeast-1",
+      Key: "number.json",
+    })
+    .promise();
 };
 
 module.exports = { save };
